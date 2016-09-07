97f1c1aa03bdfcb420ec8c0fc308449d8c4e1aec
HenrikPrangel <henrik.prangel@gmail.com>
2016-09-07 16:36:58 +0300

    Add files via upload

---
+++ praktikum1/TERE.java
@@ -0,0 +1,11 @@
+package praktikum1;
+
+public class TERE {
+	public static void main(String[] kasurida) {
+		System.out.println("***** *****  ****   *****  *");
+		System.out.println(" *    *      *   *  *      *");
+		System.out.println(" *    *****  ****   *****  *");
+		System.out.println(" *    *      * *    *      ");
+		System.out.println(" *    *****  *  *   *****  *");
+	}
+}
